1162aa1387b2d7dc06d973da9c4a440376607862
xl.Jin <ellobodking@gmail.com>
2024-10-16 21:43:21 +0800

    add Slf4j

--- demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplication.java
+++ demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplication.java
@@ -1,11 +1,18 @@
 package com.xkcoding.helloworld;
 
 import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * <p>
@@ -17,10 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@Slf4j
 public class SpringBootDemoHelloworldApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoHelloworldApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+      ConfigurableApplicationContext context = SpringApplication.run(SpringBootDemoHelloworldApplication.class, args);
+      // 获取app的 访问地址
+      Environment environment = context.getBean(Environment.class);
+      //environment.getProperty("server.servlet.context-path") 应用的上下文路径，也可以称为项目路径
+      String host = InetAddress.getLocalHost().getHostAddress();
+      String port = environment.getProperty("server.port");
+      String path = environment.getProperty("server.servlet.context-path");
+      if (StringUtils.isEmpty(path)) {
+        path = "";
+      }
+      log.info("\n访问地址：http://{}:{}{}", host, port, path);
     }
 
     /**
